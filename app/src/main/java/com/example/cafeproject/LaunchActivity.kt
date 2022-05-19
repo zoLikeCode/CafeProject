bf7be18c28110fb6cfc3795754f1086c53ad2e86
Nikita Zontov <user99@mpt.ru>
2022-05-19 13:26:02 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/cafeproject/LaunchActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cafeproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
